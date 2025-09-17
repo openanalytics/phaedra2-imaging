615b32fe802a02bf2fe2a04965ed58b0a1e593f1
sberberovic <sasa.berberovic@openanalytics.eu>
2025-09-17 14:59:52 +0200

    update: add custom hasCode and equals methods

--- src/main/java/eu/openanalytics/phaedra/imaging/render/ImageRenderConfig.java
+++ src/main/java/eu/openanalytics/phaedra/imaging/render/ImageRenderConfig.java
@@ -20,10 +20,12 @@
  */
 package eu.openanalytics.phaedra.imaging.render;
 
+import java.util.Arrays;
 import java.util.HexFormat;
 
 import eu.openanalytics.phaedra.imaging.util.ImageDataUtils;
 import eu.openanalytics.phaedra.imaging.util.Rectangle;
+import java.util.Objects;
 
 public class ImageRenderConfig {
 
@@ -32,23 +34,23 @@ public class ImageRenderConfig {
 	public Float scale;
 	public Rectangle region;
 	public ChannelRenderConfig[] channelConfigs;
-	
+
 	public ImageRenderConfig() {
 		// Default constructor
 	}
-	
+
 	public ImageRenderConfig(String... channels) {
 		channelConfigs = new ChannelRenderConfig[channels.length];
 		for (int i = 0; i < channels.length; i++) {
 			channelConfigs[i] = new ChannelRenderConfig(channels[i]);
 		}
 	}
-	
+
 	public ImageRenderConfig withDefaults() {
 		fillDefaults();
 		return this;
 	}
-	
+
 	public void fillDefaults() {
 		format = "jpg";
 		gamma = 1.0f;
@@ -59,7 +61,7 @@ public class ImageRenderConfig {
 			}
 		}
 	}
-	
+
 	@Override
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
@@ -71,38 +73,70 @@ public class ImageRenderConfig {
 		}
 		return sb.toString();
 	}
-	
+
 	public static class ChannelRenderConfig {
-		
+
 		public String name;
 		public Integer rgb;
 		public Float alpha;
 		public Float contrastMin;
 		public Float contrastMax;
-		
+
 		public ChannelRenderConfig() {
 			// Default constructor
 		}
-		
+
 		public ChannelRenderConfig(String name) {
 			this.name = name;
 		}
-		
+
 		public ChannelRenderConfig withDefaults() {
 			fillDefaults();
 			return this;
 		}
-		
+
 		public void fillDefaults() {
 			rgb = ImageDataUtils.encodeToInteger(255, 255, 255, 255);
 			alpha = 1.0f;
 			contrastMin = 0.0f;
 			contrastMax = 1.0f;
 		}
-		
+
 		@Override
 		public String toString() {
 			return String.format("Channel [name=%s, rgb=%s, alpha=%f, contrast=%f-%f]", name, HexFormat.of().toHexDigits(rgb), alpha, contrastMin, contrastMax);
 		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (o == null || getClass() != o.getClass()) {
+				return false;
+			}
+			ChannelRenderConfig that = (ChannelRenderConfig) o;
+			return Objects.equals(name, that.name) && Objects.equals(rgb, that.rgb)
+					&& Objects.equals(alpha, that.alpha) && Objects.equals(contrastMin,
+					that.contrastMin) && Objects.equals(contrastMax, that.contrastMax);
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(name, rgb, alpha, contrastMin, contrastMax);
+		}
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		ImageRenderConfig that = (ImageRenderConfig) o;
+		return Objects.equals(format, that.format) && Objects.equals(gamma,
+				that.gamma) && Objects.equals(scale, that.scale) && Objects.equals(region,
+				that.region) && Objects.deepEquals(channelConfigs, that.channelConfigs);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(format, gamma, scale, region, Arrays.hashCode(channelConfigs));
 	}
 }
